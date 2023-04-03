a1a99166f3642dd6b4571273be369ed22b0497c5
SaraCoelhoCosta <costa.saracoelho@gmail.com>
2023-04-03 08:12:36 -0300

    Atualização: Fazendo chamada do mapa pela main

--- main.py
+++ main.py
@@ -1,36 +1,38 @@
+import sys
 import pygame
-# from queue import PriorityQueue
-from map import make_grid, draw
+from draw_map import draw_map
+from algorithm import algorithm
+from map import get_hyrule, get_dungeon1, get_dungeon2, get_dungeon3
 
 SIZE = 672 # Tamanho da janela
 WINDOW = pygame.display.set_mode((SIZE, SIZE))  # Tamanho da janela
 pygame.display.set_caption('Zelda com A*')  # Título
 
-
-def main(win=WINDOW, size=SIZE):
-    #Rows = 50
-    #grid = make_grid(Rows, size)
+def main(win=WINDOW, size_win=SIZE):
+    hyrule = get_hyrule(size_win)
 
     run = True
     while run:
-        draw(win, grid, Rows, size)
+        draw_map(win, size_win, hyrule.size, hyrule.points_map)
 
         for event in pygame.event.get():
-            if event.type == pygame.QUIT or event.type == pygame.K_q: # Sair ou encerrar programa
-                run = False
-
             if event.type == pygame.KEYDOWN:
-                if event.type == pygame.K_SPACE:
-                    for row in grid:
-                        for point in row:
-                            point.update_neighbors(grid)
-
-                    algorithm(lambda:draw(win, grid, Rows, size), grid, start, end)
+                if event.key == pygame.K_q: # Sair ou encerrar programa
+                    run = False
+                    pygame.quit()
+                    sys.exit()
 
                 if event.key == pygame.K_c: # Limpa o mapa
-                    grid = make_grid(Rows, size)
+                    hyrule = get_hyrule(size_win)
 
-    pygame.quit()
+'''
+TODO: Fazer
+                if event.key == pygame.K_SPACE:
+                    for row in hyrule.points_map:
+                        for spot in row:
+                            spot.update_neighbors(hyrule.points_map)
 
+                    algorithm(lambda:draw_map(win, size_win, hyrule.size, hyrule.points_map), hyrule.points_map, hyrule.start, hyrule.end)
+'''
 
 main()
